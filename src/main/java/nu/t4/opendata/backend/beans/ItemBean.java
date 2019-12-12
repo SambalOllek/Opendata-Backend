30d1fa49fd4ad554fff39061d7ae44777d9aaf2b
Erik Hult <32297312+zyxago@users.noreply.github.com>
2019-12-12 13:30:17 +0100

    updated addItem, removeItem and getItems

--- src/main/java/nu/t4/opendata/backend/beans/ItemBean.java
+++ src/main/java/nu/t4/opendata/backend/beans/ItemBean.java
@@ -13,48 +13,70 @@ import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class ItemBean {
 
     public List<Item> getItems(){
+        List<Item> items = new ArrayList();
         try (Connection connection = ConnectionFactory.getConnection()){
             Statement stmt = connection.createStatement();
             String sql = "SELECT * FROM item";
             ResultSet data = stmt.executeQuery(sql);
             while (data.next()) {
-                //TODO update so that it takes all item values from db and adds to item obj
-                int itemId = data.getInt("Id");
-                String link = data.getString("link");
+                Item item = new Item();
+                item.setId(data.getInt("id"));
+                item.setAddress(data.getString("address"));
+                item.setBrand(data.getString("brand"));
+                item.setDrivewheel(data.getString("drivewheel"));
+                item.setFuel(data.getString("fuel"));
+                item.setMilage(data.getInt("milage"));
+                item.setPrice(data.getInt("price"));
+                item.setLink(data.getString("link"));
+                item.setRegnum(data.getString("regnum"));
+                item.setModel(data.getString("model"));
+                item.setGearbox(data.getString("gearbox"));
+                item.setYear(data.getInt("year"));
+                items.add(item);
             }
         } catch (Exception e) {
             //TODO: fixa logger
+            System.out.println("Error in ItemBean.getItems: " + e.getMessage());
         }
-        return null;
+        return items;
     }
 
-    Item addItem(Item item){
+    int addItem(Item item){
         try (Connection connection = ConnectionFactory.getConnection()){
-            PreparedStatement stmt = connection.prepareStatement("INSERT INTO item VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO item VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
             stmt.setString(1, item.getLink());
-            //TODO add stmt.set on all values
-            if(stmt.executeUpdate() != 0){
-                String sql = "LAST_INSERT_ID()";
-                ResultSet data = stmt.executeQuery(sql);
-                item.setId(data.getInt(1));
-            }
+            stmt.setString(2, item.getBrand());
+            stmt.setString(3, item.getRegnum());
+            stmt.setString(4, item.getAddress());
+            stmt.setString(5, item.getModel());
+            stmt.setString(6, item.getDrivewheel());
+            stmt.setInt(7, item.getMilage());
+            stmt.setInt(8, item.getPrice());
+            stmt.setString(9, item.getGearbox());
+            stmt.setInt(10, item.getYear());
+            return stmt.executeUpdate();
         } catch (Exception e) {
             //TODO: fixa logger
+            System.out.println("Error in ItemBean.addItem: " + e.getMessage());
         }
-        return null;
+        return 0;
     }
 
     int removeItem(int itemId){
         try (Connection connection = ConnectionFactory.getConnection()){
-            Statement stmt = connection.createStatement();
+            PreparedStatement stmt = connection.prepareStatement("DELETE FROM item WHERE id = ?");
+            stmt.setInt(1, itemId);
+            return stmt.executeUpdate();
         } catch (Exception e) {
             //TODO: fixa logger
+            System.out.println("Error in ItemBean.removeItem: " + e.getMessage());
         }
         return 0;
     }
