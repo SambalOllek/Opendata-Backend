ca4d27cacdd6491617c3359290414d510f1564c5
Erik Hult <32297312+zyxago@users.noreply.github.com>
2019-12-12 10:43:56 +0100

    Create AuthResource.java

---
+++ src/main/java/nu/t4/opendata/backend/resources/AuthResource.java
@@ -0,0 +1,19 @@
+package nu.t4.opendata.backend.resources;
+
+import javax.ws.rs.core.Response;
+
+public class AuthResource {
+    nu.t4.opendata.backend.beans.CredentialsBean credentialsBean;
+
+    public Response checkCredentials() {
+        return null;
+    }
+
+    public Response addCredentials() {
+        return null;
+    }
+
+    public Response checkToken() {
+        return null;
+    }
+}
