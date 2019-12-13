0cc52646815144e6a41e47a2bd8d04521b13ea00
Erik Hult <32297312+zyxago@users.noreply.github.com>
2019-12-13 14:10:23 +0100

    started on webscraping logic

--- src/main/java/nu/t4/opendata/backend/beans/WebScraperBean.java
+++ src/main/java/nu/t4/opendata/backend/beans/WebScraperBean.java
@@ -1,11 +1,29 @@
 package nu.t4.opendata.backend.beans;
 
 import nu.t4.opendata.backend.entities.Item;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import javax.ejb.Singleton;
 
+@Singleton
 public class WebScraperBean {
-    public void scrape(String url) {
+    private WebDriver webDriver;
 
+    public void scrape(String url) {
+        System.out.println("In WebScraperBean.scrape");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Erik\\Documents\\Programmering\\Opendata-Backend\\chromedriver.exe");
+        webDriver = new ChromeDriver();
+        try {
+            webDriver.get("https://images.google.se");
+            webDriver.findElement(By.className("gLFyf gsfi")).sendKeys("Teknikum");
+            webDriver.findElement(By.className("Tg7LZd")).click();
+        } catch (Exception e) {
+            System.out.println("Error in WebScraperBean.scrape: " + e.getMessage());
+        }
     }
 
     private Item buildItem() {
