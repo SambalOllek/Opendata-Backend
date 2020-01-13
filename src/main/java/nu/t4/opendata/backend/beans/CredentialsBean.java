4af7efe2117dbc2592ab2821f844a4250bfad413
Erik Hult <32297312+zyxago@users.noreply.github.com>
2020-01-13 14:16:32 +0100

    Added logic for user login, register and verifying token

--- src/main/java/nu/t4/opendata/backend/beans/CredentialsBean.java
+++ src/main/java/nu/t4/opendata/backend/beans/CredentialsBean.java
@@ -1,8 +1,15 @@
 package nu.t4.opendata.backend.beans;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.mysql.jdbc.Connection;
+import java.security.SecureRandom;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Base64;
 import nu.t4.opendata.backend.entities.Credentials;
 
 import javax.ejb.Stateless;
+import nu.t4.opendata.backend.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,15 +18,82 @@ public class CredentialsBean {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(CarBean.class);
     
-    public String validateCredentials(Credentials credentials) {
-        return "";
+
+    public Credentials createCredentials(String basicAuth) {
+        basicAuth = basicAuth.substring(6).trim();//Removes "Basic " from string
+        byte[] bytes = Base64.getDecoder().decode(basicAuth);
+        basicAuth = new String(bytes);
+        int colon = basicAuth.indexOf(":");
+        String username = basicAuth.substring(0, colon);
+        String password = basicAuth.substring(colon + 1);
+        return new Credentials(username, password);
     }
 
-    public boolean validateToken(String token) {
+    private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); //threadsafe
+
+    public static String generateNewToken() {
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
+    
+    public boolean verifyToken(String token){;
+        try (Connection connection = ConnectionFactory.getConnection()){
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM user WHERE token = ?");
+            stmt.setString(1, token);
+            ResultSet data = stmt.executeQuery();
+            if(data.next()){
+                return true;
+            }
+        }catch(Exception e){
+            LOGGER.error(e.getMessage());
+        }
         return false;
     }
+    
+    private int addToken(Credentials credentials, String token){
+        try (Connection connection = ConnectionFactory.getConnection()){
+            PreparedStatement stmt = connection.prepareStatement("UPDATE user SET token = ? WHERE username = ?");
+            stmt.setString(1, token);
+            stmt.setString(2, credentials.getUsername());
+            return stmt.executeUpdate();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+        return 0;
+    }
 
-    public int registerCredentials(Credentials credentials) {
+    public String checkCredentials(Credentials credentials) {
+        String token = "";
+        String hashedPassword = "";
+        try ( Connection connection = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM user WHERE username = ?");
+            stmt.setString(1, credentials.getUsername());
+            ResultSet data = stmt.executeQuery();
+            if (data.next()) {
+                hashedPassword = data.getString("hash");
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+        if(BCrypt.verifyer().verify(credentials.getPassword().toCharArray(), hashedPassword).verified){
+            token = generateNewToken();
+            addToken(credentials, token);
+        }
+        return token;
+    }
+
+    public int saveCredentials(Credentials credentials) {
+        try ( Connection connection = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO user (username, hash) VALUES(?, ?)");
+            stmt.setString(1, credentials.getUsername());
+            String hashedPassword = BCrypt.withDefaults().hashToString(13, credentials.getPassword().toCharArray());
+            stmt.setString(2, hashedPassword);
+            return stmt.executeUpdate();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
         return 0;
     }
 }
--- src/main/java/nu/t4/opendata/backend/resources/AuthResource.java
+++ src/main/java/nu/t4/opendata/backend/resources/AuthResource.java
@@ -1,19 +1,69 @@
 package nu.t4.opendata.backend.resources;
 
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import nu.t4.opendata.backend.beans.CredentialsBean;
+import nu.t4.opendata.backend.entities.Credentials;
 
+/**
+ * 
+ * @author Erik
+ */
+@Path("auth")
 public class AuthResource {
-    nu.t4.opendata.backend.beans.CredentialsBean credentialsBean;
 
-    public Response checkCredentials() {
-        return null;
-    }
+    @EJB
+    CredentialsBean credentialsBean;
 
-    public Response addCredentials() {
-        return null;
+    /**
+     * 
+     * @param basicAuth
+     * @return 
+     */
+    @GET
+    public Response checkUser(@HeaderParam("authorization") String basicAuth) {
+        Credentials credentials = credentialsBean.createCredentials(basicAuth);
+        String token = credentialsBean.checkCredentials(credentials);
+        if (!token.equals("")) {
+            return Response.ok(token).build();
+        } else {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
     }
+    /**
+     * 
+     * @param token
+     * @return 
+     */
+    @Path("/token")
+    @GET
+    public Response verifyUser(@HeaderParam("authorization") String token) {
+        if(credentialsBean.verifyToken(token)){
+            return Response.status(Response.Status.OK).build();
+        }
+        return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+    
+    /**
+     * 
+     * @param basicAuth
+     * @return 
+     */
+    @Path("/create")
+    @GET
+    public Response createUser(@HeaderParam("authorization") String basicAuth) {
+        Credentials credentials = credentialsBean.createCredentials(basicAuth);
+        if (credentialsBean.saveCredentials(credentials) == 1) {
+            return Response.status(Response.Status.CREATED).build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+    
 
-    public Response checkToken() {
-        return null;
-    }
 }
