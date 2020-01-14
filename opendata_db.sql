8583abc1ae98d48f15299524eb551ea171e27372
Erik Hult <32297312+zyxago@users.noreply.github.com>
2020-01-14 12:43:57 +0100

    Update opendata_db.sql

--- opendata_db.sql
+++ opendata_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 12 dec 2019 kl 10:31
+-- Tid vid skapande: 14 jan 2020 kl 11:00
 -- Serverversion: 10.4.6-MariaDB
 -- PHP-version: 7.3.9
 
@@ -27,20 +27,22 @@ USE `opendata_db`;
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `item`
+-- Tabellstruktur `car`
 --
 
-CREATE TABLE `item` (
+CREATE TABLE `car` (
   `id` int(11) NOT NULL COMMENT 'Item id (PRIMARY)',
   `link` varchar(256) NOT NULL COMMENT 'Link to bytbil.com article. ex: bytbil.com/[LINK]',
   `brand` varchar(32) NOT NULL COMMENT 'Bilens märke',
   `regnum` varchar(16) NOT NULL COMMENT 'Regnummret till bilen',
   `address` varchar(128) NOT NULL COMMENT 'Address till vart bilen finns',
   `model` varchar(32) NOT NULL COMMENT 'Bilens modell',
-  `drivewheel` varchar(16) NOT NULL COMMENT 'fwd / rwd / 4wd',
+  `drivewheel` varchar(16) NOT NULL COMMENT '2wd / 4wd',
   `milage` int(11) NOT NULL COMMENT 'Hur många mil bilen gått',
   `price` int(11) NOT NULL COMMENT 'Vad bilen säljs för',
-  `gearbox` varchar(16) NOT NULL COMMENT 'manuel / automat'
+  `gearbox` varchar(16) NOT NULL COMMENT 'manuel / automat',
+  `year` int(11) NOT NULL COMMENT 'Årsmodell',
+  `fuel` varchar(32) NOT NULL COMMENT 'Type of fuel (Disel/Bensin)'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -52,27 +54,29 @@ CREATE TABLE `item` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL COMMENT 'User ID (PRIMARY)',
   `username` varchar(32) NOT NULL COMMENT 'username (UNIQUE)',
-  `hash` varchar(256) NOT NULL COMMENT 'hashed version of users password'
+  `hash` varchar(256) NOT NULL COMMENT 'hashed version of users password',
+  `oauth_id` varchar(32) DEFAULT NULL COMMENT 'Oauth ID from Github',
+  `token` varchar(128) DEFAULT NULL COMMENT 'OAuth/Auth Token'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `user_item`
+-- Tabellstruktur `user_car`
 --
 
-CREATE TABLE `user_item` (
+CREATE TABLE `user_car` (
   `user_id` int(11) NOT NULL COMMENT 'Users ID',
-  `item_id` int(11) NOT NULL COMMENT 'Items ID'
+  `car_id` int(11) NOT NULL COMMENT 'Items ID'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Ersättningsstruktur för vy `user_item_full_info`
+-- Ersättningsstruktur för vy `user_car_full_info`
 -- (See below for the actual view)
 --
-CREATE TABLE `user_item_full_info` (
+CREATE TABLE `user_car_full_info` (
 `user_id` int(11)
 ,`username` varchar(32)
 ,`item_id` int(11)
@@ -90,20 +94,20 @@ CREATE TABLE `user_item_full_info` (
 -- --------------------------------------------------------
 
 --
--- Struktur för vy `user_item_full_info`
+-- Struktur för vy `user_car_full_info`
 --
-DROP TABLE IF EXISTS `user_item_full_info`;
+DROP TABLE IF EXISTS `user_car_full_info`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `user_item_full_info`  AS  select `user`.`id` AS `user_id`,`user`.`username` AS `username`,`item`.`id` AS `item_id`,`item`.`link` AS `link`,`item`.`brand` AS `brand`,`item`.`regnum` AS `regnum`,`item`.`address` AS `address`,`item`.`model` AS `model`,`item`.`drivewheel` AS `drivewheel`,`item`.`milage` AS `milage`,`item`.`price` AS `price`,`item`.`gearbox` AS `gearbox` from ((`user` join `item`) join `user_item`) where `user`.`id` = `user_item`.`user_id` and `item`.`id` = `user_item`.`item_id` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `user_car_full_info`  AS  select `user`.`id` AS `user_id`,`user`.`username` AS `username`,`car`.`id` AS `item_id`,`car`.`link` AS `link`,`car`.`brand` AS `brand`,`car`.`regnum` AS `regnum`,`car`.`address` AS `address`,`car`.`model` AS `model`,`car`.`drivewheel` AS `drivewheel`,`car`.`milage` AS `milage`,`car`.`price` AS `price`,`car`.`gearbox` AS `gearbox` from ((`user` join `car`) join `user_car`) where `user`.`id` = `user_car`.`user_id` and `car`.`id` = `user_car`.`car_id` ;
 
 --
 -- Index för dumpade tabeller
 --
 
 --
--- Index för tabell `item`
+-- Index för tabell `car`
 --
-ALTER TABLE `item`
+ALTER TABLE `car`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `link_idx` (`link`);
 
@@ -112,23 +116,25 @@ ALTER TABLE `item`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username_idx` (`username`);
+  ADD UNIQUE KEY `username_idx` (`username`),
+  ADD UNIQUE KEY `token_idx` (`token`),
+  ADD UNIQUE KEY `oauth_idx` (`oauth_id`);
 
 --
--- Index för tabell `user_item`
+-- Index för tabell `user_car`
 --
-ALTER TABLE `user_item`
-  ADD PRIMARY KEY (`user_id`,`item_id`),
-  ADD KEY `item_id` (`item_id`);
+ALTER TABLE `user_car`
+  ADD PRIMARY KEY (`user_id`,`car_id`),
+  ADD KEY `item_id` (`car_id`);
 
 --
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
 --
--- AUTO_INCREMENT för tabell `item`
+-- AUTO_INCREMENT för tabell `car`
 --
-ALTER TABLE `item`
+ALTER TABLE `car`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Item id (PRIMARY)';
 
 --
@@ -142,10 +148,10 @@ ALTER TABLE `user`
 --
 
 --
--- Restriktioner för tabell `user_item`
+-- Restriktioner för tabell `user_car`
 --
-ALTER TABLE `user_item`
-  ADD CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`),
+ALTER TABLE `user_car`
+  ADD CONSTRAINT `item_id` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`),
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
